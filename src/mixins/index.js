1c4299b949c805607fecacc2349e27a69f69db9d
yanghui <yanghui@youkb.com>
2018-03-01 11:03:31 +0800

    添加mixins

---
+++ src/mixins/index.js
@@ -0,0 +1,14 @@
+export default {
+  methods: {
+    jump(to){
+      if (this.$router) {
+        this.$router.push(to)
+      }
+    },
+    // replace(to){
+    //   if(this.$router){
+    //     this.$router.replace(to)
+    //   }
+    // }
+  }
+}
